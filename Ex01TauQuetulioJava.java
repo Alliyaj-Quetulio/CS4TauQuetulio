13c03e9af21c20a1c2d8cec04c1ceeebbc289566
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-09-07 08:52:18 +0800

    Add files via upload

---
+++ Ex01TauQuetulioJava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex01.tau.quetulio.java;
+
+/**
+ *
+ * @author TAU
+ */
+public class Ex01TauQuetulioJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name1 = "Alliyaj Quetulio";
+        String name2 = "Sheena Sahagun";
+        String name3 = "Claire Chua";
+        
+        int age1 = 15;
+        int age2 = 15;
+        int age3 = 16;
+        
+        String sec1 = "Tau";
+        String sec2 = "Tau";
+        String sec3 = "Tau";
+        
+        System.out.printf("Student 1 \nName: %s \nAge: %s \nSection: %s \n \nStudent 2 \nName: %s \nAge: %s", name1, age1, sec1, name2, age2);
+        
+    }
+    
+}
