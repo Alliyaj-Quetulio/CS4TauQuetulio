5420f076ed3d6f3e1345ed4f9b0bc54098f294e3
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-10-04 08:42:40 +0800

    Create Audience.java

---
+++ Q1Ex4/Audience.java
@@ -0,0 +1,25 @@
+package ex04tauquetulio;
+// @author TAU - Quetulio
+
+public class Audience {
+    String name, ticket;
+    int fanYrs, enjoyment, streams;
+    Song favSong;
+    
+    
+    public Audience(String name){
+        String[] tOptions = {"Regular", "VIP"};
+        int random1 = (int)Math.floor(Math.random()*(2)+0);
+        int random2 = (int)Math.floor(Math.random()*(10)+1);
+        
+        this.name = name;
+        this.ticket = tOptions[random1];
+        this.fanYrs = random2;
+        this.streams = 0;
+    }
+    
+    public void display() {
+        System.out.printf("[%s] %nTicket: %s %nYears as a fan: %s %nNumber of streams done: %s %n %n", name, ticket, fanYrs, streams);
+    }
+}
+
