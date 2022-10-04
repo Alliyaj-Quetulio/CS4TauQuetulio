c40c5d301d8a430b8b45609c7b5c282acef531f2
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-10-04 08:30:06 +0800

    Create Item.java

---
+++ Lectures/Item.java
@@ -0,0 +1,16 @@
+package lecture;
+
+/**
+ *
+ * @author TAU
+ */
+public class Item {
+    String name;
+    int price;
+    
+    Item(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+    
+}
