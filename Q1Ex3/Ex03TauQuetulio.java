57e68d9237117a08d119500c38f7b14645431321
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-10-09 00:15:03 +0800

    Create Ex03TauQuetulio.java

---
+++ Q1Ex3/Ex03TauQuetulio.java
@@ -0,0 +1,45 @@
+package ex03tauquetulio;
+// @author TAU - Quetulio
+
+public class Ex03TauQuetulio {
+
+    public static void main(String[] args) {
+        
+        // three instances of the Audience class
+        Audience reese = new Audience("Reese");
+        Audience anika = new Audience("Anika");
+        Audience quenso = new Audience("Quenso");
+        
+        // two instances of the Song class
+        Song song1 = new Song("All Too Well");
+        Song song2 = new Song("Like or Like Like");
+       
+        // one instace of the Singer class
+        Singer mxmtoon = new Singer("mxmtoon", "Frown");
+        
+        // displays the details for each instance of every class
+        System.out.println("!!! CONCERT DETAILS !!! \n");
+        
+        System.out.println("(1) AUDIENCE MEMBER(S):");
+        reese.display();
+        anika.display();
+        quenso.display();
+        
+        System.out.println("(2) SONG(S):");
+        song1.display();
+        song2.display();
+        
+        System.out.println("(3) SINGER(S):");
+        mxmtoon.display();
+        
+        // displays the results of executed methods
+        System.out.println("!!! CONCERT EVENTS !!! \n");
+
+        mxmtoon.performForAudience(12, mxmtoon.favoriteSong);
+        mxmtoon.changeFavSong("Unspoken Words");
+        
+        song1.streamSong(reese);
+        song2.streamSong(anika);
+    }
+    
+}
