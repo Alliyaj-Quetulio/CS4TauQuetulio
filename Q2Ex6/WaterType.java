bcc94efe0c200e9816c8bf92c4a4b639cb04bdcd
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-11-17 00:02:38 +0800

    Update WaterType.java

--- Q2Ex6/WaterType.java
+++ Q2Ex6/WaterType.java
@@ -4,13 +4,14 @@ public class WaterType extends Monster {
     
     WaterType(String n, int m, int b) {
         super(n, "water", "fire", "grass", m, b);
-        atk = b*(int)0.7;
-        def = b*(int)1.3;
+        atk = (b*70)/100;
+        def = (b*130)/100;
     }
     
     @Override
     public void special() {
         def += 2;
         hp -= (maxHP*0.10);
+        System.out.printf("%s did a pose, increasing it to %s DEF and reducing it to %sHP. %n", name, getDef(), getHP());
     }
 }
