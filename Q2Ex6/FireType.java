f12059462a0bc14381089271c5f58c1496aa1235
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-11-17 00:00:32 +0800

    Update FireType.java

--- Q2Ex6/FireType.java
+++ Q2Ex6/FireType.java
@@ -4,13 +4,14 @@ public class FireType extends Monster {
     
     FireType(String n, int m, int b) {
         super(n, "fire", "grass", "water", m, b);
-        atk = b*(int)1.3;
-        def = b*(int)0.7;
+        atk = (b*130)/100;
+        def = (b*70)/100;
     }
     
     @Override
     public void special() {
         atk += 2;
         hp -= (maxHP*0.10);
+        System.out.printf("%s did a pose, increasing it to %s ATK and reducing it to %sHP. %n", name, getAtk(), getHP());
     }
 }
