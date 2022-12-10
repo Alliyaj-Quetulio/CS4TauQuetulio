8088393f1d60d283f6414048fedab4cf90870fcc
Alliyaj-Quetulio <97627070+Alliyaj-Quetulio@users.noreply.github.com>
2022-12-10 14:32:36 +0800

    Update Exercise06tauquetuliomarraalliyajnicole.java

--- Q2Ex6/Exercise06tauquetuliomarraalliyajnicole.java
+++ Q2Ex6/Exercise06tauquetuliomarraalliyajnicole.java
@@ -1,67 +1,16 @@
-package exercise06tauquetuliomarraalliyajnicole;
+package exercise07tauquetuliomarraalliyajnicole;
 
-public class Exercise06tauquetuliomarraalliyajnicole {
-
+public class Exercise07tauquetuliomarraalliyajnicole {
+    
     public static void main(String[] args) {
         
-        FireType reese = new FireType("Reese", 50, 40);
-        GrassType sheena = new GrassType("Sheena", 50, 40);
-        WaterType anika = new WaterType("Anika", 50, 40);
+        Trainer t1 = new Trainer("Nicole");
+        FireType m1 = new FireType("Gary", 100, 50);
+        NPC n1 = new NPC("Ella");
+        Location l1 = new Location("PISAY", "Reqs");
         
-        boolean fight = true;
-        
-        reese.introduce();
-        sheena.introduce();
-        anika.introduce();
-        
-        System.out.println();
-        System.out.println("[Battle 1: Grass vs. Fire]");
-        while (fight) {
-            sheena.attack(reese);
-            if (reese.getHP() == 0) {
-                fight = false;
-                break;
-            }
-            reese.attack(sheena);
-            if (sheena.getHP() == 0) {
-                fight = false;
-            }
-        }
-        sheena.restoreHealth();
-        reese.restoreHealth();
-        
-        fight = true;
-        System.out.println();
-        System.out.println("[Battle 2: Fire vs. Water]");
-        while (fight) {
-            reese.attack(anika);
-            if (anika.getHP() == 0) {
-                fight = false;
-                break;
-            }
-            anika.attack(reese);
-            if (reese.getHP() == 0) {
-                fight = false;
-            }
-        }
-        reese.restoreHealth();
-        anika.restoreHealth();
-        
-        fight = true;
-        System.out.println();
-        System.out.println("[Battle 3: Water vs. Grass]");
-        while (fight) {
-            anika.attack(sheena);
-            if (sheena.getHP() == 0) {
-                fight = false;
-                break;
-            }
-            sheena.attack(anika);
-            if (anika.getHP() == 0) {
-                fight = false;
-            }
-        }
-        anika.restoreHealth();
-        sheena.restoreHealth();      
-    }
+        t1.inspect(m1);
+        t1.inspect(n1);
+        t1.inspect(l1);
+    }   
 }
